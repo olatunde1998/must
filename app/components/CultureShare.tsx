f309ed31377b1dbc654c41e488a83a7c97cfd0f1
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:23:01 +0100

    feat: enhance CultureShare component with motion effects for improved user experience

--- app/components/CultureShare.tsx
+++ app/components/CultureShare.tsx
@@ -1,9 +1,18 @@
+"use client";
+import { motion } from "framer-motion";
 import Image from "next/image";
 
 export default function CultureShare() {
   return (
     <>
-      <section className="max-w-2xl mx-auto px-4 py-16 md:py-32">
+      <motion.section
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.2 }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", duration: 3 }}
+        className="max-w-2xl mx-auto px-4 py-16 md:py-32"
+      >
         <p className="font-bold text-xl text-center md:text-5xl md:leading-16 lg:leading-20">
           새로운 문화 나눔을 만들어갑니다.
         </p>
@@ -11,16 +20,28 @@ export default function CultureShare() {
           <p className="font-bold text-xl md:text-5xl md:leading-16 lg:leading-20">
             우리는
           </p>
-          <Image
-            src="/assets/book.png"
-            alt="Book Image"
-            width={280}
-            height={94}
-            sizes="100vw"
-            priority
-            quality={100}
-            className="object-contain w-fit h-[45px] md:h-[94px] rounded-full"
-          />
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false, amount: 0.2 }}
+            whileHover={{
+              scale: 1.05,
+              boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+            }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", duration: 3 }}
+          >
+            <Image
+              src="/assets/book.png"
+              alt="Book Image"
+              width={280}
+              height={94}
+              sizes="100vw"
+              priority
+              quality={100}
+              className="object-contain w-fit h-[45px] md:h-[94px] rounded-full cursor-pointer"
+            />
+          </motion.div>
         </div>
         <p className="font-bold text-2xl md:text-5xl md:leading-16 lg:leading-20 text-center">
           사색의향기입니다.
@@ -30,7 +51,7 @@ export default function CultureShare() {
           사회 공헌과 선진적이고 건전한 사회 문화를 만들어나가기 위해 노력하는
           비영리 단체입니다.
         </p>
-      </section>
+      </motion.section>
     </>
   );
 }
