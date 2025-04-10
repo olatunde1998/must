7e25c2e2a69fab1c4583a459699b28477319f46f
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:23:26 +0100

    feat: add motion effects to SendDonation component for enhanced interactivity

--- app/components/SendDonation.tsx
+++ app/components/SendDonation.tsx
@@ -1,9 +1,22 @@
+"use client";
 import { HeartHandshake } from "lucide-react";
+import { motion } from "framer-motion";
 
 export default function SendDonation() {
   return (
     <>
-      <section className="bg-[#F8F4F3] flex flex-col justify-center items-center px-6 py-16 space-y-6">
+      <motion.section
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.2 }}
+        whileHover={{
+          scale: 1.05,
+          boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+        }}
+        animate={{ x: 0 }}
+        transition={{ type: "spring", duration: 3 }}
+        className="bg-[#F8F4F3] flex flex-col justify-center items-center px-6 py-16 space-y-6"
+      >
         <p>기부를 통해 따뜻한 마음을 보내고 싶다면,</p>
         <p>후원계좌 안내</p>
         <p>국민 924501-01-351204 / 사단법인 사색의향기</p>
@@ -16,7 +29,7 @@ export default function SendDonation() {
             일시 후원
           </button>
         </div>
-      </section>
+      </motion.section>
     </>
   );
 }
