45176f4b727baf8648919b5368e32f6c9c2a6de6
geodevcode <geodevinstitute@gmail.com>
2025-04-10 02:47:26 +0100

    feat: reorganize imports and update Home component to include EffortsToCreate, Companions, and Footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import Hero from "@/app/components/Hero";
 import CultureShare from "@/app/components/CultureShare";
 import ChairPersonIntro from "@/app/components/ChairPersonIntro";
 import CollaborativeWork from "@/app/components/CollaborativeWork";
+import EffortsToCreate from "@/app/components/EffortsToCreate";
 import SendDonation from "@/app/components/SendDonation";
+import Companions from "@/app/components/Companions";
+import Footer from "@/app/components/Footer";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
   return (
@@ -12,6 +15,9 @@ export default function Home() {
       <ChairPersonIntro />
       <CollaborativeWork />
       <SendDonation />
+      <EffortsToCreate />
+      <Companions />
+      <Footer />
     </div>
   );
 }
