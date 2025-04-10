236a87ded1702af88b4cfaefd7d1d40c8355ebca
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:26:54 +0100

    feat: update test to render Hero component and verify welcome message

--- __test__/page.test.tsx
+++ __test__/page.test.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Home from "@/app/page";
+import Hero from "@/app/components/Hero";
 
 describe("Dashboard Page Component", () => {
   test("Should renders Properly", () => {
-    const { getByText } = render(<Home />);
-    // Test for the welcome message
-    const welcomeMessage = getByText("Save and see your changes instantly.");
+    const { getByText } = render(<Hero />);
+    // Test for the Hero Component
+    const welcomeMessage = getByText("함께하는 마음이 만드는 소중한 변화");
     expect(welcomeMessage).toBeInTheDocument();
   });
 });
