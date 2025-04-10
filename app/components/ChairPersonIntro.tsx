a61357a71bf53c236e4f322e879b7b7b10872d77
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:22:37 +0100

    feat: enhance ChairPersonIntro component with motion effects and dynamic founders data

--- app/components/ChairPersonIntro.tsx
+++ app/components/ChairPersonIntro.tsx
@@ -1,12 +1,22 @@
+"use client";
 import { ChevronRight } from "lucide-react";
+import { motion } from "framer-motion";
 import Image from "next/image";
+import { foundersData } from "@/utils/FoundersData";
 
 export default function ChairPersonIntro() {
   return (
     <>
       <section className="max-w-7xl mx-auto px-4 md:px-24">
         <div className="flex flex-col-reverse md:flex-row items-end justify-between gap-20">
-          <div className="max-w-[500px] mb-12">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false, amount: 0.2 }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", duration: 3 }}
+            className="max-w-[500px] mb-12"
+          >
             <p className="text-2xl md:text-5xl md:leading-16 lg:leading-20 mb-10">
               이사장 인사말.
             </p>
@@ -21,16 +31,18 @@ export default function ChairPersonIntro() {
             </div>
             <div className="mt-6 flex items-center">
               <p>사단법인 사색의향기 이사장 오덕균</p>
-              <Image
-                src="/assets/signature.png"
-                alt="Signature Image"
-                width={187}
-                height={79}
-                sizes="100vw"
-                priority
-                quality={100}
-                className="object-contain w-fit h-[79px]"
-              />
+              <div>
+                <Image
+                  src="/assets/signature.png"
+                  alt="Signature Image"
+                  width={187}
+                  height={79}
+                  sizes="100vw"
+                  priority
+                  quality={100}
+                  className="object-contain w-fit h-[79px]"
+                />
+              </div>
             </div>
             <button className="flex items-center gap-3 cursor-pointer">
               인터뷰 더 보기{" "}
@@ -38,8 +50,19 @@ export default function ChairPersonIntro() {
                 <ChevronRight />
               </span>
             </button>
-          </div>
-          <div className="xl:mr-[-175px]">
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: false, amount: 0.2 }}
+            whileHover={{
+              scale: 1.05,
+              boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+            }}
+            animate={{ x: 0 }}
+            transition={{ type: "spring", duration: 3 }}
+            className="xl:mr-[-175px]"
+          >
             <Image
               src="/assets/chairman-image.png"
               alt="Chairman Image"
@@ -50,55 +73,39 @@ export default function ChairPersonIntro() {
               quality={100}
               className="object-contain w-fit"
             />
-          </div>
+          </motion.div>
         </div>
 
         <div className="space-y-10 md:space-y-0 md:flex justify-between md:gap-10 md:mt-32 max-w-5xl">
-          <div className="flex flex-col items-center">
-            <Image
-              src="/assets/deokgyun-avatar.png"
-              alt="Chairperson Avatar Image"
-              width={270}
-              height={270}
-              sizes="100vw"
-              priority
-              quality={100}
-              className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
-            />
-            <p className="mt-3 text-base md:text-lg font-semibold">
-              오덕균 이사장
-            </p>
-          </div>
-          <div className="flex flex-col items-center">
-            <Image
-              src="/assets/junho-avatar.png"
-              alt="Director Avatar Image"
-              width={270}
-              height={270}
-              sizes="100vw"
-              priority
-              quality={100}
-              className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
-            />
-            <p className="mt-3 text-base md:text-lg font-semibold">
-              정준호 이사
-            </p>
-          </div>
-          <div className="flex flex-col items-center">
-            <Image
-              src="/assets/hyukjun-avatar.png"
-              alt="Advisor Avatar Image"
-              width={270}
-              height={270}
-              sizes="100vw"
-              priority
-              quality={100}
-              className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
-            />
-            <p className="mt-3 text-base md:text-lg font-semibold">
-              권혁준 고문
-            </p>
-          </div>
+          {foundersData.map((item, index) => (
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              viewport={{ once: false, amount: 0.2 }}
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+              }}
+              animate={{ x: 0 }}
+              transition={{ type: "spring", duration: 3 }}
+              key={index}
+              className="flex flex-col items-center"
+            >
+              <Image
+                src={item.imageUrl}
+                alt="Chairperson Avatar Image"
+                width={270}
+                height={270}
+                sizes="100vw"
+                priority
+                quality={100}
+                className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
+              />
+              <p className="mt-3 text-base md:text-lg font-semibold">
+                {item.name}
+              </p>
+            </motion.div>
+          ))}
         </div>
       </section>
     </>
