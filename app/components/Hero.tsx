cfd57cbb383d2051e2d753431eebae04697a1655
geodevcode <geodevinstitute@gmail.com>
2025-04-10 02:49:08 +0100

    feat: update Hero component layout and enhance styling with new image and button elements

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,5 +1,5 @@
-import { FaWhatsapp } from "react-icons/fa";
 import Image from "next/image";
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 export default function Hero() {
   return (
@@ -7,18 +7,18 @@ export default function Hero() {
       <div className="bg-[#F8F7F3]">
         {/* ======HERO SECTION====== */}
         <section className="">
-          <div className="relative rounded-xl">
+          <div className="relative rounded-br-full">
             <Image
-              src="/assets/must-prototype.png"
+              src="/assets/hero-image.png"
               alt="Hero Background Image"
               width={1440}
               height={800}
               sizes="100vw"
               priority
               quality={100}
-              className="h-[750px] w-full lg:h-[850px] lg:w-[1440] xl:w-full object-cover"
+              className="h-[750px] w-full lg:h-[780px] lg:w-[1440] xl:w-full object-cover rounded-br-[180px]"
             />
-            <div className="flex flex-col justify-between  md:px-8 lg:px-24 pt-64 absolute inset-0 bg-black/60">
+            <div className="flex flex-col justify-between  md:px-8 lg:px-24 pt-64 absolute inset-0 bg-black/40">
               <div className="px-4 text-center text-[#FFFFFF] mt-10  max-w-7xl mx-auto">
                 <p className="text-base p-3 md:text-3xl mt-3 mb-3 max-w-2xl mx-auto">
                   특별한 나눔
@@ -30,6 +30,10 @@ export default function Hero() {
                   즐거움과 나눔이 있는 감동의 순간
                 </p>
               </div>
+            </div>
+            <div className="rounded-t-4xl flex items-center justify-end space-x-2 bg-white p-4  absolute -bottom-10 right-0 w-[300px]">
+              <ArrowLeft className="size-10 bg-[#111111] text-white p-2 rounded-full cursor-pointer" />
+              <ArrowRight className="size-10 bg-[#111111] text-white p-2 rounded-full cursor-pointer" />
             </div>
           </div>
         </section>
