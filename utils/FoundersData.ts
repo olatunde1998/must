9eb176379a7e01ecf8dccfa283ef5d1b5a3bf920
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:21:19 +0100

    feat: add founders data with images and names

---
+++ utils/FoundersData.ts
@@ -0,0 +1,23 @@
+import ChairmanAvatar from "@/public/assets/deokgyun-avatar.png";
+import DirectorAvatar from "@/public/assets/junho-avatar.png";
+import AdvisorAvatar from "@/public/assets/hyukjun-avatar.png";
+
+
+//Founder data
+export const foundersData = [
+  {
+    id: 1,
+    imageUrl: ChairmanAvatar,
+    name: "오덕균 이사장",
+  },
+  {
+    id: 2,
+    imageUrl: DirectorAvatar,
+    name: "정준호 이사",
+  },
+  {
+    id: 3,
+    imageUrl: AdvisorAvatar,
+    name: "권혁준 고문",
+  },
+];
