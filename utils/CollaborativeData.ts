2adc6b45e20295314f2dd1af3bce151e82bbb3ed
geodevcode <geodevinstitute@gmail.com>
2025-04-10 08:21:56 +0100

    feat: add collaborative data with project images and descriptions

---
+++ utils/CollaborativeData.ts
@@ -0,0 +1,36 @@
+import ProjectOne from "@/public/assets/work-one-image.png";
+import ProjectTwo from "@/public/assets/work-two-image.png";
+import ProjectThree from "@/public/assets/work-three-image.png";
+import ProjectFour from "@/public/assets/work-four-image.png";
+
+//Collaborative data
+export const collaborativeData = [
+  {
+    id: 1,
+    imageUrl: ProjectOne,
+    headings: "인문학 콘서트",
+    content:
+      "깊은 사유와 따뜻한 대화가 어우러진 시간 마음을 울리는 문학과  인문학의 이야기",
+  },
+  {
+    id: 2,
+    imageUrl: ProjectTwo,
+    headings: "인문학 콘서트",
+    content:
+      "깊은 사유와 따뜻한 대화가 어우러진 시간 마음을 울리는 문학과 인문학의 이야기",
+  },
+  {
+    id: 3,
+    imageUrl: ProjectThree,
+    headings: "인문학 콘서트",
+    content:
+      "깊은 사유와 따뜻한 대화가 어우러진 시간 마음을 울리는 문학과 인문학의 이야기",
+  },
+  {
+    id: 4,
+    imageUrl: ProjectFour,
+    headings: "인문학 콘서트",
+    content:
+      "  깊은 사유와 따뜻한 대화가 어우러진 시간 마음을 울리는 문학과 인문학의 이야기",
+  },
+];
