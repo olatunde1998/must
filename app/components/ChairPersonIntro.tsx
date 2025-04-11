f48147f420cf394f8348db4cc87a161d3f401a79
geodevcode <geodevinstitute@gmail.com>
2025-04-11 11:22:06 +0100

    feat: enhance ChairPersonIntro component with internationalization support and dynamic content rendering

--- app/components/ChairPersonIntro.tsx
+++ app/components/ChairPersonIntro.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { ChevronRight } from "lucide-react";
+import { useTranslations } from "next-intl";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import { foundersData } from "@/utils/FoundersData";
 
 export default function ChairPersonIntro() {
+  const t = useTranslations("ChairPersonIntroSection");
+  const keys = ["memberOne", "memberTwo", "memberThree"];
   return (
     <>
       <section className="max-w-7xl mx-auto px-6 md:px-24">
@@ -18,19 +20,16 @@ export default function ChairPersonIntro() {
             className="max-w-[500px] mb-12"
           >
             <p className="text-2xl md:text-5xl md:leading-16 lg:leading-20 mb-10">
-              이사장 인사말.
+              {/* 이사장 인사말. */}
+              {t("contentOne")}
             </p>
             <div className="text-sm">
-              <p>안녕하세요, 사색의향기 이사장 오덕균 입니다.</p>
-              <p className="my-2.5">
-                사색의향기는 ‘행복한 문화 나눔’을 통해 더 나은 사회를
-                만들어가고자 노력해왔습니다. 앞으로도 문학과 인문학의 가치를
-                공유하며, 열린 커뮤니티로서 함께 성장하겠습니다.
-              </p>
-              <p>많은 관심과 응원 부탁드립니다. 감사합니다.</p>
+              <p>{t("contentTwo")}</p>
+              <p className="my-2.5">{t("contentThree")}</p>
+              <p>{t("contentFour")}</p>
             </div>
             <div className="mt-6 flex items-center">
-              <p>사단법인 사색의향기 이사장 오덕균</p>
+              <p>{t("contentFive")}</p>
               <div>
                 <Image
                   src="/assets/signature.png"
@@ -45,7 +44,7 @@ export default function ChairPersonIntro() {
               </div>
             </div>
             <button className="flex items-center gap-3 cursor-pointer">
-              인터뷰 더 보기{" "}
+              {t("contentSix")}
               <span>
                 <ChevronRight />
               </span>
@@ -77,35 +76,38 @@ export default function ChairPersonIntro() {
         </div>
 
         <div className="space-y-10 md:space-y-0 md:flex justify-between md:gap-10 md:mt-32 max-w-5xl">
-          {foundersData.map((item, index) => (
-            <motion.div
-              initial={{ opacity: 0, y: 20 }}
-              whileInView={{ opacity: 1, y: 0 }}
-              viewport={{ once: false, amount: 0.2 }}
-              whileHover={{
-                scale: 1.05,
-                boxShadow: "0px 0px 8px rgb(255, 255, 255)",
-              }}
-              animate={{ x: 0 }}
-              transition={{ type: "spring", duration: 3 }}
-              key={index}
-              className="flex flex-col items-center"
-            >
-              <Image
-                src={item.imageUrl}
-                alt="Chairperson Avatar Image"
-                width={270}
-                height={270}
-                sizes="100vw"
-                priority
-                quality={100}
-                className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
-              />
-              <p className="mt-3 text-base md:text-lg font-semibold">
-                {item.name}
-              </p>
-            </motion.div>
-          ))}
+          {keys.map((item, index) => {
+            const imageUrl = t(`teamMember.${item}.imageUrl`);
+            return (
+              <motion.div
+                initial={{ opacity: 0, y: 20 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                viewport={{ once: false, amount: 0.2 }}
+                whileHover={{
+                  scale: 1.05,
+                  boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+                }}
+                animate={{ x: 0 }}
+                transition={{ type: "spring", duration: 3 }}
+                key={index}
+                className="flex flex-col items-center"
+              >
+                <Image
+                  src={imageUrl}
+                  alt="Chairperson Avatar Image"
+                  width={270}
+                  height={270}
+                  sizes="100vw"
+                  priority
+                  quality={100}
+                  className="object-contain w-fit max-h-[200px] md:max-h-none lg:h-[200px] rounded-full bg-gray-300"
+                />
+                <p className="mt-3 text-base md:text-lg font-semibold">
+                  {t(`teamMember.${item}.name`)}
+                </p>
+              </motion.div>
+            );
+          })}
         </div>
       </section>
     </>
