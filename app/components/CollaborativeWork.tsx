a36d67eee4c689bea28dc596c69c33af543556bf
geodevcode <geodevinstitute@gmail.com>
2025-04-11 11:22:13 +0100

    feat: refactor CollaborativeWork component for improved internationalization and dynamic content rendering

--- app/components/CollaborativeWork.tsx
+++ app/components/CollaborativeWork.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { collaborativeData } from "@/utils/CollaborativeData";
+// import { collaborativeData } from "@/utils/CollaborativeData";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function CollaborativeWork() {
+  const t = useTranslations("CollaborativeWorkSection");
+  const keys = ["projectOne", "projectTwo", "projectThree", "projectFour"];
   return (
     <>
       <section
@@ -12,92 +15,98 @@ export default function CollaborativeWork() {
       >
         <div className="flex gap-10 justify-center items-center mb-6">
           <p className="text-2xl md:text-4xl md:leading-16 lg:leading-20">
-            함께하는 일
+            {t("contentOne")}
           </p>
           <p className="text-2xl md:text-4xl md:leading-16 lg:leading-20 text-[#BBBBBB]">
-            우리의 흔적
+            {t("contentTwo")}
           </p>
         </div>
         <main className="space-y-8">
-          {collaborativeData.map((item, index) => (
-            <section
-              key={item.id}
-              className="lg:flex justify-betwee lg:items-end lg:gap-10"
-            >
-              {index % 2 === 0 ? (
-                <>
-                  <div className="flex flex-col-reverse gap-6 lg:flex-row lg:items-end">
-                    <motion.div
-                      initial={{ opacity: 0, y: 20 }}
-                      whileInView={{ opacity: 1, y: 0 }}
-                      viewport={{ once: false, amount: 0.2 }}
-                      whileHover={{
-                        scale: 1.05,
-                        boxShadow: "0px 0px 8px rgb(255, 255, 255)",
-                      }}
-                      animate={{ x: 0 }}
-                      transition={{ type: "spring", duration: 3 }}
-                      className="lg:w-[70%]"
-                    >
-                      <Image
-                        src={item.imageUrl}
-                        alt="Collaborative work image"
-                        width={752}
-                        height={131}
-                        sizes="100vw"
-                        priority
-                        quality={100}
-                        className="object-cover  w-fit h-[131px] lg:h-[151px]"
-                      />
-                    </motion.div>
+          {keys.map((item, index) => {
+            const imageUrl = t(`pastProject.${item}.imageUrl`);
+            const title = t(`pastProject.${item}.title`);
+            const subTitle = t(`pastProject.${item}.subTitle`);
 
-                    <div className="lg:max-w-[30%]">
-                      <p className="text-base md:my-4">{item.content}</p>
-                      <p className="font-bold text-2xl lg:text-3xl">
-                        {item.headings}
-                      </p>
+            return (
+              <section
+                key={index}
+                className="lg:flex justify-betwee lg:items-end lg:gap-10"
+              >
+                {index % 2 === 0 ? (
+                  <>
+                    <div className="flex flex-col-reverse gap-6 lg:flex-row lg:items-end">
+                      <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        whileHover={{
+                          scale: 1.05,
+                          boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+                        }}
+                        animate={{ x: 0 }}
+                        transition={{ type: "spring", duration: 3 }}
+                        className="lg:w-[70%]"
+                      >
+                        <Image
+                          src={imageUrl}
+                          alt="Collaborative work image"
+                          width={752}
+                          height={131}
+                          sizes="100vw"
+                          priority
+                          quality={100}
+                          className="object-cover  w-fit h-[131px] lg:h-[151px]"
+                        />
+                      </motion.div>
+
+                      <div className="lg:max-w-[30%]">
+                        <p className="text-base md:my-4">{subTitle}</p>
+                        <p className="font-bold text-2xl lg:text-3xl">
+                          {title}
+                        </p>
+                      </div>
                     </div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="flex flex-col gap-6 lg:flex-row lg:items-end">
-                    <div className="lg:max-w-[30%]">
-                      <p className="lg:text-right text-base md:my-4">
-                        {item.content}
-                      </p>
-                      <p className="lg:text-right font-bold text-2xl lg:text-3xl">
-                        {item.headings}
-                      </p>
+                  </>
+                ) : (
+                  <>
+                    <div className="flex flex-col gap-6 lg:flex-row lg:items-end">
+                      <div className="lg:max-w-[30%]">
+                        <p className="lg:text-right text-base md:my-4">
+                          {subTitle}
+                        </p>
+                        <p className="lg:text-right font-bold text-2xl lg:text-3xl">
+                          {title}
+                        </p>
+                      </div>
+                      <motion.div
+                        initial={{ opacity: 0, y: 20 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: false, amount: 0.2 }}
+                        whileHover={{
+                          scale: 1.05,
+                          boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+                        }}
+                        animate={{ x: 0 }}
+                        transition={{ type: "spring", duration: 3 }}
+                        className="lg:w-[70%]"
+                      >
+                        <Image
+                          src={imageUrl}
+                          alt="Collaborative work image"
+                          width={752}
+                          height={131}
+                          sizes="100vw"
+                          priority
+                          quality={100}
+                          className="object-cover  w-fit h-[131px] lg:h-[151px]"
+                        />
+                      </motion.div>
                     </div>
-                    <motion.div
-                      initial={{ opacity: 0, y: 20 }}
-                      whileInView={{ opacity: 1, y: 0 }}
-                      viewport={{ once: false, amount: 0.2 }}
-                      whileHover={{
-                        scale: 1.05,
-                        boxShadow: "0px 0px 8px rgb(255, 255, 255)",
-                      }}
-                      animate={{ x: 0 }}
-                      transition={{ type: "spring", duration: 3 }}
-                      className="lg:w-[70%]"
-                    >
-                      <Image
-                        src={item.imageUrl}
-                        alt="Collaborative work image"
-                        width={752}
-                        height={131}
-                        sizes="100vw"
-                        priority
-                        quality={100}
-                        className="object-cover  w-fit h-[131px] lg:h-[151px]"
-                      />
-                    </motion.div>
-                  </div>
-                </>
-              )}
-            </section>
-          ))}
+                  </>
+                )}
+              </section>
+            );
+          })}
         </main>
       </section>
     </>
