4757a96f98f93d83f1de5d9ecb430803b9b1dd99
geodevcode <geodevinstitute@gmail.com>
2025-04-11 12:12:59 +0100

    refactor: simplify MobileNavbar component by removing unused state and imports

--- app/components/navbar/MobileNavbar.tsx
+++ app/components/navbar/MobileNavbar.tsx
@@ -1,18 +1,15 @@
 "use client";
-import { FaBars } from "react-icons/fa";
-import { useState } from "react";
+import LanguageSwitcher from "@/app/components/LanguageSwitcher";
+import BrandLogo from "@/public/assets/must-brand-logo.png";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function MobileNavbar() {
-  const [nav, setNav] = useState(false);
-  const handleClick = () => setNav(!nav);
-
   return (
     <div className="fixed w-full h-[80px] z-[1000] flex justify-between items-center px-4 bg-white backdrop-blur-10 backdrop-blur-md  text-gray-300 lg:hidden">
       <Link href="/">
         <Image
-          src="/assets/must-brand-logo.png"
+          src={BrandLogo}
           alt="Must Brand logo"
           width={200}
           height={76}
@@ -22,19 +19,9 @@ export default function MobileNavbar() {
           className="object-contain w-fit h-[30px]"
         />
       </Link>
-
-      {/* ====Hamburger===== */}
-      {/* {!nav && ( */}
-      <div
-        onClick={handleClick}
-        className="lg:hidden z-10 bg-gray-200 p-2 rounded-lg text-[#29190E]"
-      >
-        <FaBars size={24} />
+      <div className="bg-[#ffffff] h-fit md:hidden text-[#212121]">
+        <LanguageSwitcher />
       </div>
-      {/* )} */}
-
-      {/* ====SideBar  list==== */}
-      {/* {nav && <SideBar handleClick={() => handleClick()} />} */}
     </div>
   );
 }
