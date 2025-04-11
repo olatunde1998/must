32bed059e1fab506dac72b4004d943509f83cade
geodevcode <geodevinstitute@gmail.com>
2025-04-11 13:58:13 +0100

    feat: add companion data with images and descriptions

---
+++ utils/CompanionData.ts
@@ -0,0 +1,32 @@
+import CompanionOne from "@/public/assets/companion-image-one.png";
+import CompanionTwo from "@/public/assets/companion-image-two.png";
+import CompanionThree from "@/public/assets/companion-image-three.png";
+import CompanionFour from "@/public/assets/companion-image-four.png";
+
+//Companion data
+export const companionData = [
+  {
+    id: 1,
+    imageUrl: CompanionOne,
+    headings: "(주)행복한사람들",
+    content: "행복한 날, 행복한 사람들과 함께하는 특별한 날을 선사합니다.",
+  },
+  {
+    id: 2,
+    imageUrl: CompanionTwo,
+    headings: "바른댓글 실천연대",
+    content: "바른댓글 정착으로 아름다운 사회를 만들어갑니다.",
+  },
+  {
+    id: 3,
+    imageUrl: CompanionThree,
+    headings: "아트리안",
+    content: "생활미술시장의 뉴 패러다임을 제안하는 상설 아트갤러리 프로젝트.",
+  },
+  {
+    id: 4,
+    imageUrl: CompanionFour,
+    headings: "대한민국위멘위원회",
+    content: "어린이를 위한 위대한 멘토, 위멘이 함께 합니다.",
+  },
+];
