dbafc859c2754faef0eeac489f486b9ba956d18c
geodevcode <geodevinstitute@gmail.com>
2025-04-11 11:22:37 +0100

    feat: implement internationalization for SendDonation component

--- app/components/SendDonation.tsx
+++ app/components/SendDonation.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { HeartHandshake } from "lucide-react";
 import { motion } from "framer-motion";
+import { useTranslations } from "next-intl";
 
 export default function SendDonation() {
+  const t = useTranslations("SendDonationSection");
   return (
     <>
       <motion.section
@@ -17,16 +19,16 @@ export default function SendDonation() {
         transition={{ type: "spring", duration: 3 }}
         className="bg-[#F8F4F3] flex flex-col justify-center items-center px-6 py-16 space-y-6"
       >
-        <p>기부를 통해 따뜻한 마음을 보내고 싶다면,</p>
-        <p>후원계좌 안내</p>
-        <p>국민 924501-01-351204 / 사단법인 사색의향기</p>
+        <p>{t("contentOne")}</p>
+        <p>{t("contentTwo")}</p>
+        <p>{t("contentThree")}</p>
         <div className="flex items-center gap-6">
           <button className="rounded-3xl text-base border-[1.3px] border-gray-400 flex items-center gap-3 px-3 py-2 cursor-pointer">
             <HeartHandshake className="size-4" />
-            정기 후원
+            {t("contentFour")}
           </button>
           <button className="rounded-3xl text-base border-[1.3px] border-gray-400 px-3 py-2 cursor-pointer">
-            일시 후원
+            {t("contentFive")}
           </button>
         </div>
       </motion.section>
