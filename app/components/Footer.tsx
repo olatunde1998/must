c2a77b550e3a35c490bb478701a34f420e8efb45
geodevcode <geodevinstitute@gmail.com>
2025-04-11 11:23:17 +0100

    feat: implement internationalization for Footer component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,9 @@
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function Footer() {
+  const t = useTranslations("FooterSection");
   return (
     <>
       <footer className="bg-[#222222] pb-24 md:pb-0">
@@ -18,61 +20,57 @@ export default function Footer() {
               className="object-contain w-fit h-[50px] hidden lg:block"
             />
             <div className="mt-4 flex flex-col md:flex-row  md:mt-8 lg:mt-20 mb-10 text-xs md:text-sm xl:text-base leading-6 space-x-6">
-              <span>개인정보 처리방침</span>
-              <span>이용약관</span>
-              <span className="text-[#7C7C7C]">
-                고객센터 평일 08:00~19:00 (주말 및 공휴일 휴무)
-              </span>
+              <span>{t("contentOne")}</span>
+              <span>{t("contentTwo")}</span>
+              <span className="text-[#7C7C7C]">{t("contentThree")}</span>
             </div>
             <div className="text-xs flex flex-col md:flex-row items-center space-y-3 space-x-6">
-              <p className="mr-auto">
-                사단법인 사색의향기
+              <p className="mr-auto">{t("contentFour")}</p>
+              <p className="space-x-3 flex flex-row mr-auto justify-start items-center">
+                <span className="text-[#7C7C7C]">{t("contentFive")}</span>
+                <span>{t("contentSix")}</span>
               </p>
               <p className="space-x-3 flex flex-row mr-auto justify-start items-center">
-                <span className="text-[#7C7C7C]">이사장</span>
-                <span>오덕균</span>
-              </p>
-              <p className="space-x-3 flex flex-row mr-auto justify-start items-center">
-                <span className="text-[#7C7C7C]">고유번호</span>
-                <span>355-82-00129</span>
+                <span className="text-[#7C7C7C]">{t("contentSeven")}</span>
+                <span>{t("contentEight")}</span>
               </p>
             </div>
 
             <div className="text-xs flex flex-col md:flex-row items-center justify-start space-y-3 md:space-y-0 space-x-6 my-3">
               <div className="space-x-3 flex flex-row mr-auto justify-start items-center md:flex-row">
-                <p className="text-[#7C7C7C]">주소</p>
-                <p>서울시 강남구 테헤란로 19길 28 이림빌딩3층(역삼동)</p>
+                <p className="text-[#7C7C7C]">
+                  주소
+                  {t("contentNine")}
+                </p>
+                <p>{t("contentTen")}</p>
               </div>
               <div className="space-x-3 flex flex-row mr-auto justify-start items-center">
-                <p className="text-[#7C7C7C]">전화</p>
-                <p>02-539-5101</p>
+                <p className="text-[#7C7C7C]">{t("contentEleven")}</p>
+                <p>{t("contentTwelve")}</p>
               </div>
               <div className="space-x-3 flex flex-row mr-auto justify-start items-center">
-                <p className="text-[#7C7C7C]">팩스</p>
-                <p>070-8668-5091</p>
+                <p className="text-[#7C7C7C]">{t("contentThirteen")}</p>
+                <p>{t("contentFourteen")}</p>
               </div>
               <div className="space-x-3 flex flex-row mr-auto justify-start items-center">
-                <p className="text-[#7C7C7C]">이메일</p>
-                <p>culppy@culppy.com</p>
+                <p className="text-[#7C7C7C]">{t("contentFifteen")}</p>
+                <p>{t("contentSixteen")}</p>
               </div>
             </div>
-            <p className="text-xs">
-              Copyright ⓒ 2025 사색의향기. All Rights Reserved
-            </p>
+            <p className="text-xs">{t("contentSeventeen")}</p>
           </div>
           <div className="lg:w-[30%]">
-            <p className="text-2xl mb-4 font-semibold">뉴스레터 구독하기</p>
-            <p>
-              이메일 구독을 통해 사색의향기의 최신 소식과 교육 및 이벤트 소식을
-              빠르게 받아보실 수 있습니다.
+            <p className="text-2xl mb-4 font-semibold">
+              {t("contentEighteen")}
             </p>
+            <p>{t("contentNineteen")}</p>
             <div className="border-1 border-slate-200 mt-12 rounded-full p-1.5 md:p-2.5 w-full">
               <form action="">
                 <div className="flex items-center">
                   <input
                     type="email"
                     name="email"
-                    placeholder="이메일 구독신청"
+                    placeholder={t("contentTwenty")}
                     className="w-[85%] p-2 focus:outline-none  placeholder:text-xs placeholder:text-white rounded-full"
                   />
                   <ArrowRight />
