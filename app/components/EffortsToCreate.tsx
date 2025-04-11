5a336de2c801ad816bf93bf6a707e2a3adafa071
geodevcode <geodevinstitute@gmail.com>
2025-04-11 11:22:32 +0100

    feat: add internationalization support to EffortsToCreate component

--- app/components/EffortsToCreate.tsx
+++ app/components/EffortsToCreate.tsx
@@ -2,8 +2,10 @@
 import { Download } from "lucide-react";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function EffortsToCreate() {
+  const t = useTranslations("EffortsToCreateSection");
   return (
     <>
       <section
@@ -30,31 +32,26 @@ export default function EffortsToCreate() {
               className="object-contain w-[40px] lg:w-fit mb-4"
             />
             <p className="text-2xl font-medium md:text-4xl md:leading-16 lg:leading-16 mb-10">
-              더 나은 세상을 <br />
-              만들어 가기 위한 노력.
+              {t("contentOne")}
+              <br />
+              {t("contentTwo")}
             </p>
             <div className="text-sm space-y-6">
               <p className="leading-6">
-                바른 인성과 성품을 고양하며 함께 나누는 활동을 통해 세대와
-                나라를 초월한 커뮤니티를 만들어나가려고 노력하고 있습니다.{" "}
-                <br /> 작은 담소와 삶의 지혜가 필요하신 모든 분들을 환영합니다.
+                {t("contentThree")}
+                <br />
+                {t("contentFour")}
               </p>
-              <p>
-                삶의 어려운 고충이 있으신 분들에게 작은 도움을 드립니다.
-                법률적이거나 개인적으로 해결하기 어려운 문제들이 있으시다면 소담
-                서포터즈에게 도움을 요청해주세요.
-              </p>
+              <p>{t("contentFive")}</p>
             </div>
 
             <p className="text-base my-16">
-              2025. 6. 출시 예정{" "}
-              <span className="ml-4 text-[#8C5947]">
-                커뮤니티 APP ‘소소담담’
-              </span>
+              {t("contentSix")}
+              <span className="ml-4 text-[#8C5947]">{t("contentSeven")}</span>
             </p>
 
             <button className="mt-8 rounded-3xl text-base border-[1.3px] border-gray-400 flex items-center gap-3 px-3 py-2 cursor-pointer">
-              정기 후원
+              {t("contentEight")}
               <Download className="size-4" />
             </button>
           </motion.div>
